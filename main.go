703c2925400bc3d03cf3e547758ff80870064425
yongjoon <penubokim@gmail.com>
2022-04-19 21:25:58 +0900

    Add Room to separate game sessions

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 	"net/http"
+	"strings"
 
 	"github.com/gorilla/websocket"
 )
@@ -12,17 +13,33 @@ var upgrader = websocket.Upgrader{
 	WriteBufferSize: 1024,
 }
 
+type Room map[string]ClientSet
+
 type ClientSet map[*websocket.Conn]bool
 
 func main() {
-	var clientSet = make(ClientSet)
+	var room = make(Room)
 
 	upgrader.CheckOrigin = func(r *http.Request) bool {
 		return true
 	}
 
-	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
-		handleWS(clientSet, w, r)
+	http.HandleFunc("/ws/", func(w http.ResponseWriter, r *http.Request) {
+		paths := strings.Split(r.URL.Path, "/")
+		var roomHash = paths[len(paths)-1]
+		client, ok := room[roomHash]
+		if !ok {
+			log.Println("new client")
+			client = make(ClientSet)
+			room[roomHash] = client
+		}
+
+		if len(room[roomHash]) >= 2 {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+
+		handleWS(client, w, r)
 	})
 
 	log.Println("Websocket server started...")
